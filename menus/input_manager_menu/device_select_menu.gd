7e1dfa919192b1a765b2c7ae187e174177d93292
groak <groakthefrog@gmail.com>
2022-10-08 16:49:35 -0500

    Updated to newer version of input manager

--- menus/input_manager_menu/device_select_menu.gd
+++ menus/input_manager_menu/device_select_menu.gd
@@ -12,27 +12,21 @@ func get_selected_device_ids()->PoolIntArray:
 func update_options()->void:
 	var popup_menu: PopupMenu = get_popup()
 	popup_menu.clear()
-	# add non-joypad first
-	for id in GGInput.get_non_joypad_devices():
+	for id in GGInput.get_device_ids():
 		var idx: int = popup_menu.get_item_count()
-		popup_menu.add_check_item(GGInput.NON_JOYPAD_DEVICE_NAMES[id])
-		popup_menu.set_item_metadata(idx, id) # set metadata as device id
-	# add joypads
-	for id in Input.get_connected_joypads():
-		var idx: int = popup_menu.get_item_count()
-		popup_menu.add_check_item('joypad %d "%s"' % [id, Input.get_joy_name(id)])
+		popup_menu.add_check_item(GGInput.get_device_name(id))
 		popup_menu.set_item_metadata(idx, id) # set metadata as device id
 
 
 func _ready()->void:
-	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+	GGInput.connect("device_connection_changed", self, "_on_device_connection_changed")
 	var popup := get_popup()
 	popup.connect("index_pressed", self, "_on_index_pressed")
 	popup.hide_on_checkable_item_selection = false
 	update_options()
 
 
-func _on_joy_connection_changed(_d:int, _c:bool)->void:
+func _on_device_connection_changed(_d:int, _c:bool)->void:
 	update_options()
 
 
