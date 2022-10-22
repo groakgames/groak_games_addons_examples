18c14e8c507be3688b638b69688fcc46aa95ba4b
groak <groakthefrog@gmail.com>
2022-10-22 06:51:14 -0500

    Updated to handle mouse support

--- scenes/2d/example_character.gd
+++ scenes/2d/example_character.gd
@@ -11,15 +11,22 @@ var _move_input: Vector2
 
 func _ready()->void:
 	Gin.connect_input_unhandled(player_id, "gameplay_move", self, "_on_gameplay_move")
-
+	Gin.connect_input(player_id, "gameplay_move", self, "_on_gameplay_move_")
 
 func _physics_process(delta:float)->void:
 	translate(delta * SPEED * _move_input)
 
 
+func _on_gameplay_move_(input:Vector2, event:InputEvent, is_absoulute:bool)->void:
+	if is_absoulute:
+		print(input)
+		position = input
+	else:
+		translate(input)
+
 func _on_gameplay_move(input:Vector2, event:InputEvent, is_absoulute:bool)->void:
 	if is_absoulute:
-		pass
+		print(input)
 	else:
 		_move_input = input
 
